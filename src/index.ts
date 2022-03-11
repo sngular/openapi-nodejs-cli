adf8ea682e771a22c69287e533580d8bd328248c
Jorge Mayoral Álvarez <jmayoral@corunet.com>
2022-03-11 10:38:44 +0100

    Implement CLI

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-import {generateClientCode} from "./client";
+import {program} from 'commander'
+import {generateClientCode} from "./client"
 
-const urls = ['http://127.0.0.1:8080/kafka.yaml', 'http://127.0.0.1:8080/schemaRegistry.yaml']
-const allowedPaths: string[] = []
+program
+    .version('0.0.1')
+    .requiredOption('-u, --urls <string...>', 'OpenAPI spec URLs')
+    .option('--allowed-paths <string...>', 'list of allowed paths from spec')
+    .option('--client', 'only generate client code' )
+    .option('--server', 'only generate server code (not implemented yet)')
 
-generateClientCode(urls, allowedPaths).then(_ => {
-})
+program.parse()
+const options = program.opts()
+
+console.log(options)
+
+const urls = options.urls
+const allowedPaths: string[] = options.allowedPaths
+
+if (!options.client && !options.server) {
+    options.client = true
+    options.server = true
+}
+
+if (options.client) {
+    generateClientCode(urls, allowedPaths).then(_ => {
+    })
+}
