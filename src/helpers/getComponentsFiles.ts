17a1d6d6fbc9add38ccdc9cf196d91f9e5549f95
Julio César Abalde Reyes <julio.abalde@sngular.com>
2022-05-06 09:52:18 +0200

    #1 Fix components file read from local drive

--- src/helpers/getComponentsFiles.ts
+++ src/helpers/getComponentsFiles.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
 import fs from "fs";
-import path from "path";
 
 export async function getComponentsFiles(filePath: string, isUrl: boolean) {
   if (isUrl) {
     let response = await axios.get(`http://localhost:8080/${filePath}`);
     return response.data;
   }
-
-  return fs.readFileSync(path.join(__dirname, filePath), "utf-8");
+  return fs.readFileSync(filePath, "utf-8");
 }
