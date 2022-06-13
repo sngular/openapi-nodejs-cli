f20a5425cac506f3d5a18ef26d41483a2e54e820
Julio César Abalde Reyes <julio.abalde@sngular.com>
2022-06-13 16:06:36 +0200

    #1 Adds license info to all source code files

--- src/helpers/divideIntoDocumentsByTag.ts
+++ src/helpers/divideIntoDocumentsByTag.ts
@@ -1,3 +1,8 @@
+/*
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 import { EMPTY_TAG } from "../const";
 import { DataObject, FileObject, Method, Path } from "../types";
 import { parseDocument } from "./documentParser";
--- src/helpers/resolveSchemaName.ts
+++ src/helpers/resolveSchemaName.ts
@@ -1,3 +1,8 @@
+/*
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 /**
  * @description If the input starts with '#', returns an array containing the strings required to access the schema
  * @param {string} refValue
